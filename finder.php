28a222d4a66dff48d73d134799798909a3faf153
Azimjon Kamolov <azimjonkamolovuz@gmail.com>
2020-05-11 09:43:06 +0900

    done

--- finder.php
+++ finder.php
@@ -42,52 +42,79 @@
 
         }
 
+        // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
         // echo "<h1> IMAGE URLS ARE HERE </h1>";
         // echo "<br>";
 
+        // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
         echo  "<h1> " . $i . " image urls are found  </h1><br>";
+        $val = 0;
 
         for($n = 0; $n < $i; $n++)
         {
 
-            
+            if( strpos($links[$n], ".png") !== false || strpos($links[$n], ".jpg") !== false || strpos($links[$n], ".jpeg") !== false)
+            {
 
-            if ($links[$n][0] == '/' && $links[$n][1] != '/')
-            {
-                $link = $links[$n] = $url.$links[$n];
-                echo "<a href='$link'>" . $link . "</a>";
-            }
-            else if ($links[$n][0] == 'h' && $links[$n][1] == 't')
-            {
-                $link =  $links[$n];
+                $val = $val + 1;
 
-                echo "<a href='$link'>" . $link . "</a>";
-            }
-            else if ($links[$n][0] == '#')
-            {
-                continue;
+                if ($links[$n][0] == '/' && $links[$n][1] != '/')
+                {
+                    $link = $links[$n] = $url.$links[$n];
+                    echo "<a href='$link'>" . $link . "</a>";
+                }
+                else if ($links[$n][0] == 'h' && $links[$n][1] == 't')
+                {
+                    $link =  $links[$n];
+
+                    echo "<a href='$link'>" . $link . "</a>";
+                }
+                else if ($links[$n][0] == '#')
+                {
+                    continue;
+                }
+                else if ($links[$n][0] == '/' && $links[$n][1] == '/')
+                {
+                    $link = "https://" . $links[$n];
+
+                    // $link = substr($link, 2);
+
+                    // $link = "https://" . $link;
+
+                    echo "<a href='$link'>" . $link . "</a>";
+
+                }
+                else
+                {
+                    $link =  $links[$n];
+
+                    // $link = substr($link, 2);
+
+                    // $link = "https://" . $link;
+
+                    echo "<a href='$link'>" . $link . "</a>";
+                }
             }
             else
             {
-                $link =  $links[$n];
-
-                // $link = substr($link, 2);
-
-                // $link = "https://" . $link;
-
-                echo "<a href='$link'>" . $link . "</a>";
-
+                continue;
             }
+            
                 echo "<br>";
         }
+
+        echo "<br><br>";
+        echo  "<h1> " . $val . " found image urls are valuable </h1><br>";
     }
 
-    
+    // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
     // echo "<h1> SITE URLS ARE HERE </h1>";
     // echo "<br>";
 
-    
+    // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
     function getSiteUrl($url)
     {
@@ -110,20 +137,24 @@
         }
     
         echo  "<h1> " . $t . " site urls are found  </h1><br>";
+        
 
         for($n = 0; $n < $t; $n++)
         {
-    
+            
             if ($sitelinks[$n][0] == '/')
             {
                 $link = $sitelinks[$n] = $url.$sitelinks[$n];
                 echo "<a href='$link'>" . $link . "</a>";
+                // getSiteUrl($link);
             }
             else if ($sitelinks[$n][0] == 'h' && $sitelinks[$n][1] == 't')
             {
                 $link =  $sitelinks[$n];
     
                 echo "<a href='$link'>" . $link . "</a>";
+
+                // getSiteUrl($link);
             }
             else if ($sitelinks[$n][0] == '#')
             {
@@ -138,6 +169,8 @@
                 // $link = "https://" . $link;
     
                 echo "<a href='$link'>" . $link . "</a>";
+
+                // getSiteUrl($link);
     
             }
             echo "<br>";
